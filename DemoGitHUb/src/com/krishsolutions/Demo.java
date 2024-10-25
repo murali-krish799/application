85dbcbf47472fd2bccc131117ebe28822f5149b8
murali-krish799 <krishnamurali1062001@gmail.com>
2024-10-25 16:09:11 +0530

    fdfdd

--- DemoGitHUb/src/com/krishsolutions/Demo.java
+++ DemoGitHUb/src/com/krishsolutions/Demo.java
@@ -3,6 +3,6 @@ package com.krishsolutions;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("this is for all-updated");
+		System.out.println("this is for all-updated ");
 	}
 }
