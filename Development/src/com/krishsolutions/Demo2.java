dd3596c6935056cdd10fa8c8f2a83e858cf293da
murali-krish799 <krishnamurali1062001@gmail.com>
2024-10-25 16:23:31 +0530

    new development

--- Development/src/com/krishsolutions/Demo2.java
+++ Development/src/com/krishsolutions/Demo2.java
@@ -5,5 +5,6 @@ public class Demo2 {
 	public static void main(String[] args) {
 		System.out.println("thinis ");
 	System.out.println("this is third time commit");
+	System.out.println("this is for development");
 	}
 }
