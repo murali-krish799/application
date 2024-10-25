f3cc52f9a875fda3703815a93fe0fff98371d939
murali-krish799 <krishnamurali1062001@gmail.com>
2024-10-25 16:26:18 +0530

    new method has been added

--- Development/src/com/krishsolutions/Demo2.java
+++ Development/src/com/krishsolutions/Demo2.java
@@ -4,7 +4,12 @@ public class Demo2 {
 
 	public static void main(String[] args) {
 		System.out.println("thinis ");
-	System.out.println("this is third time commit");
-	System.out.println("this is for development");
+		System.out.println("this is third time commit");
+		System.out.println("this is for development");
+	}
+
+	public void testDemo() {
+
+		System.out.println("this is for you..");
 	}
 }
