b8c8eb00ded13fafa9850e5aa782ccdc848baf7d
murali-krish799 <krishnamurali1062001@gmail.com>
2024-10-25 16:14:19 +0530

    demo-updated

--- DemoGitHUb/src/com/krishsolutions/Demo.java
+++ DemoGitHUb/src/com/krishsolutions/Demo.java
@@ -4,6 +4,6 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("this is for all-updated   ");
-		System.out.println("checking...");
+		System.out.println("checking..  .");
 	}
 }
