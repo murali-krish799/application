489c80406865c561c862df7fcfce3f11bcfdeba1
murali-krish799 <krishnamurali1062001@gmail.com>
2024-10-26 14:40:28 +0530

    this is new change from dev branch

--- ConstructorDemo/src/com/krishsolutions/defaultdemo/Client.java
+++ ConstructorDemo/src/com/krishsolutions/defaultdemo/Client.java
@@ -5,10 +5,18 @@ public class Client {
 
 		ConstructorDemo constructorPractice = new ConstructorDemo(30);
 
- int i = constructorPractice.i;
-		  System.out.println("this is variable value " + i);
-		   
-		 
+		int i = constructorPractice.i;
+		System.out.println("this is variable value " + i);
+
 	}
 
+	
+	
+	
+	
+	
+	public void ancientMethod() {
+
+		System.out.println("this is ancient method");
+	}
 }
