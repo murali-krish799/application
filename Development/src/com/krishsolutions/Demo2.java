ec33eee00679c4b299192ae55756ec41165a370d
murali-krish799 <krishnamurali1062001@gmail.com>
2024-10-26 14:35:04 +0530

    this is new commit

--- Development/src/com/krishsolutions/Demo2.java
+++ Development/src/com/krishsolutions/Demo2.java
@@ -9,7 +9,11 @@ public class Demo2 {
 	}
 
 	public void testDemo() {
+<<<<<<< HEAD
 //comments here main master branch
+=======
+//merging commit here
+>>>>>>> caa531681253432d3a4dbf3630205d3a9c456dc6
 		System.out.println("this is for you..");
 	}
 }
